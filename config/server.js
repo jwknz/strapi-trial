51845453f4b450b92ac5d550dd9e3eadc9240f37
Jeff Kranenburg <jk@hcs.kiwi>
2022-09-04 19:08:39 +1200

    update 2

--- config/server.js
+++ config/server.js
@@ -1,6 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://localhost'),
+  proxy: true,
+  admin: {
+    url: env('ADMIN', 'http://localhost/admin'),
+    autoOpen: false
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
